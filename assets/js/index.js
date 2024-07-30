8f18ffbec1b9bfddcc1aa229f55f4f7f409c1f96
美兰十三 <luna_yj@hotmail.com>
2024-07-30 19:26:19 +0800

    feat: tag page

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,21 @@
 
 // Import CSS
 import '../css/index.scss';
+import 'highlight.js/styles/base16/hardcore.min.css';
+// import 'highlight.js/styles/base16/humanoid-dark.min.css';
 import '../css/luno/index.scss';
-
 // Import JS
 import menuOpen from './menuOpen';
 import infiniteScroll from './infiniteScroll';
+import hljs from 'highlight.js';
+import LineNumberPlugin from '@taufik-nurrohman/highlight.ln.js';
 
 // Call the menu and infinite scroll functions
 menuOpen();
 infiniteScroll();
+
+hljs.addPlugin(LineNumberPlugin);
+hljs.highlightAll();
+// hljs.initLineNumbersOnLoad({
+//   singleLine: true,
+// });
